61062ae8e95a83411d8460ee9ecd63f0b4f758e8
Roeland <roelandsteevan@gmail.com>
2021-03-09 12:39:37 +0100

    route to overview wokring

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fetch = require('node-fetch');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app
+    .use(express.static('public'))
+    .set('view engine', 'ejs')
+
+    //routes
+    .get('/', (req, res) => {
+        res.render('pages/overview')
+    })
+    .get('/:id', (req, res) => {
+        res.render('pages/detail')
+    })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
