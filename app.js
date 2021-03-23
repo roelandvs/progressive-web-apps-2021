a4ed6d74b818d33798b3a880416cac5312f65a52
Roeland <roelandsteevan@gmail.com>
2021-03-23 15:26:29 +0100

    compression

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const compression = require('compression');
 
 const { dataTransformation } = require('./js/data/dataTransformation');
 const { detailDataTransformation } = require('./js/data/detailDataTransformation');
 
 app
     .use(express.static('public'))
+    .use(compression())
     .set('view engine', 'ejs')
 
 //routes
