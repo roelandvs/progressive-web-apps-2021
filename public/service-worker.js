091549ea6ffc61a48ae17a60638110550a356722
Roeland <roelandsteevan@gmail.com>
2021-03-31 14:22:23 +0200

    clean sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,19 @@
-const cacheName = 'v2';
+const cacheName = 'v1';
 const cacheAssets = [
     '/css/style.css',
     '/js/bundle.min.js',
     '/offline'
 ];
 
-
 //call install event
 self.addEventListener('install', e => {
-    // console.log('Service Worker: Installed');
+    console.log('Service Worker: Installed');
 
     e.waitUntil(
         caches
             .open(cacheName)
             .then(cache => {
-                // console.log('Service Worker: Caching Files');
+                console.log('Service Worker: Caching Files');
                 cache.addAll(cacheAssets);
             })
             .then(() => self.skipWaiting())
@@ -23,7 +22,7 @@ self.addEventListener('install', e => {
 
 //call activate event to clean up old cache
 self.addEventListener('activate', e => {
-    // console.log('Service Worker: Activated');
+    console.log('Service Worker: Activated');
 
     //remove unwanted caches
     e.waitUntil(
@@ -31,7 +30,7 @@ self.addEventListener('activate', e => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== cacheName) {
-                        // console.log('Service Worker: Clearing Ol Cache');
+                        console.log('Service Worker: Clearing Ol Cache');
                         return caches.delete(cache);
                     }
                 })
@@ -43,25 +42,6 @@ self.addEventListener('activate', e => {
 //call fetch event for offline pages
 self.addEventListener('fetch', event => {
 
-    // e.respondWith(
-    //     fetch(e.request)
-    //         .then(res => {
-    //             //make clone of response
-    //             const resClone = res.clone();
-    //             //open cache
-    //             caches
-    //                 .open(cacheName)
-    //                 .then(cache => {
-    //                     //add response to cache
-    //                     cache.put(e.request, resClone);
-    //                 });
-                
-    //             return res;
-    //         })
-    //         .catch(err => caches.match(e.request)
-    //             .then(res => res))
-    // )
-
     event.respondWith(
         caches.open(cacheName).then(cache => {
             return cache.match(event.request)
@@ -72,90 +52,16 @@ self.addEventListener('fetch', event => {
                     }
 
                     return fetch(event.request)
-
                     .then(response => {
                         cache.put(event.request, response.clone())
                         return response
                     })
 
-                }).catch((err) => {
-                    return caches.open(cacheName).then(cache => cache.match('/offline'))
+                })
+                .catch((err) => {
+                    return caches.match('/offline')
                 })
         })
-
     )
-    // if (isCoreGetRequest(e.request)) {
-    //     console.log('core request');
-    //     e.respondWith(
-    //         caches
-    //             .open(cacheAssets)
-    //             .then(cache => {
-    //                 cache.match(e.request.url)
-    //             }))
-    // } else {
-    //     console.log('geen core request');
-
-    //     e.respondWith(
-    //         fetch(e.request)
-    //             .then(res => {
-    //                 //make clone of response
-    //                 const resClone = res.clone();
-    //                 //open cache
-    //                 caches
-    //                     //cache assets
-    //                     .open(cacheName)
-    //                     .then(cache => {
-    //                         //add response to cache
-    //                         cache.put(e.request, resClone);
-    //                     });
-                    
-    //                 return res;
-    //             })
-
-    //             .catch((err) => {
-    //                 console.log(err)
-    //                 return caches
-    //                     .open(cacheName)
-    //                     .then(cache => cache.match('/offline'))
-    //             })
-    //     )
-
-
-        // if (isCoreGetRequest(e.request)) {
-        //     console.log('core request');
-        //     caches.match(e.request)
-        //         .then(res => res)
-        // } else {
-        //     console.log('no');
-        //     e.respondWith(
-        //         fetch(e.request)
-        //             .then(res => {
-        //                 //make clone of response
-        //                 const resClone = res.clone();
-        //                 //open cache
-        //                 caches
-        //                     .open(cacheName)
-        //                     .then(cache => {
-        //                         //add response to cache
-        //                         cache.put(e.request, resClone);
-        //                     });
-                        
-        //                 return res;
-        //             })
-        //             .catch(err => caches.match(e.request)
-        //                 .then(res => res))
-        //     )
-        // }
 });
-
-function isCoreGetRequest(request) {
-    return (
-        request.method === "GET" && cacheAssets.includes(getPathName(request.url))
-    );
-};
-
-function getPathName(requestUrl) {
-    const url = new URL(requestUrl);
-    return url.pathname;
-};
 
